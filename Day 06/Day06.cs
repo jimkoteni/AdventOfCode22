194c230e7590d4c15c189de87a88bc7d2a0a5267
Зеленский Дмитрий Александрович <zelenskij.da@esphere.ru>
2022-12-07 21:42:22 +0300

    Added Issue 6: Tuning Trouble

---
+++ Day 06/Day06.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode22.Day_06
+{
+	public class Day06 : DayBase
+	{
+		protected override string RunFirstIssue()
+		{
+			return GetDistinctSequenceIndex(4);
+		}
+
+		protected override string RunSecondIssue()
+		{
+			return GetDistinctSequenceIndex(14);
+		}
+		
+		private string GetDistinctSequenceIndex(int sequenceLength)
+		{
+			var source = File.ReadAllLines(fileName).Single();
+
+			for (var i = 0; i < source.Length - sequenceLength; i++)
+			{
+				var slice = new HashSet<char>(source[i .. (i + sequenceLength)]);
+
+				if (slice.Count == sequenceLength)
+					return (i + sequenceLength).ToString();
+			}
+
+			return 0.ToString();
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -5,9 +5,11 @@ using AdventOfCode22.Day_02;
 using AdventOfCode22.Day_03;
 using AdventOfCode22.Day_04;
 using AdventOfCode22.Day_05;
+using AdventOfCode22.Day_06;
 
 new Day01().Run();
 new Day02().Run();
 new Day03().Run();
 new Day04().Run();
 new Day05().Run();
+new Day06().Run();
